8ba8a72c7f8dd066e45b803849fc7813808ef458
Hvbris <44342309+hvbr1s@users.noreply.github.com>
2024-04-19 10:00:01 +0200

    Update semantic_updater.py

--- pinecone_pipeline/update_scripts/semantic_updater.py
+++ pinecone_pipeline/update_scripts/semantic_updater.py
@@ -1,7 +1,6 @@
 import time
 import json
 import os
-from os import path
 from dotenv import load_dotenv
 from tqdm.auto import tqdm
 from pinecone import Pinecone
@@ -12,13 +11,23 @@ load_dotenv()
 
 # Initialize Pinecone client
 pinecone_key = os.environ['PINECONE_API_KEY']
+
+# Prod DB
 index_name = 'serverless-test'
-pc_host = 'https://serverless-test-e865e64.svc.apw5-4e34-81fa.pinecone.io'
+pc_host = 'https://serverless-test-e865e64.svc.apw5-4e34-81fa.pinecone.io' 
+# Backup DB
+backup_index_name = 'prod'
+backup_pc_host = 'https://prod-e865e64.svc.northamerica-northeast1-gcp.pinecone.io'
+
 pc = Pinecone(api_key=pinecone_key)
 index = pc.Index(
         index_name,
         host=pc_host
-    )
+)
+backup_index = pc.Index(
+        backup_index_name,
+        host=backup_pc_host
+)
 
 # Initialize OpenAI client
 client = OpenAI()
@@ -61,12 +70,28 @@ def run_updater(json_file_path:str = None):
                     vectors=to_upsert, 
                     namespace='eng'
                 ) 
+                index_stats_response = index.describe_index_stats()
+                print(f'\nMain database updated! -> {index_stats_response}')
+
+                backup_index.upsert(
+                    vectors=to_upsert,
+                    namespace='eng'
+                )
+                backup_index_stats_response = backup_index.describe_index_stats()
+                print(f'\nBackup database updated! -> {backup_index_stats_response}')
             except:
-                time.sleep(120)
+                print(f'Oops something went wrong, trying again!')
+                time.sleep(15)
                 index.upsert(
                     vectors=to_upsert, 
                     namespace='eng'
+                )
+                print(f'Main database updated!')
+                backup_index.upsert(
+                    vectors=to_upsert,
+                    namespace='eng'
                 ) 
+                print(f'Backup database updated!')
  
         except Exception as e:
             print(f"Failed to upsert the following data: {to_upsert}")
@@ -74,11 +99,8 @@ def run_updater(json_file_path:str = None):
             failed_chunks += 1  # Increment the counter for each failed chunk
             continue
 
-    print('Database updated!')
     if failed_chunks > 0:  # If there are any failed chunks, print the count
         print(f"Total number of failed chunks: {failed_chunks}")
-    index_stats_response = index.describe_index_stats()
-    print(index_stats_response)
 
 if __name__ == "__main__":
     run_updater()
